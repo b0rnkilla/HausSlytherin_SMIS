c62825e0cada6f06ac591fc7bb76e9582f71bfdc
Andreas Lorenz <andreaslorenz82@googlemail.com>
2026-03-11 17:11:35 +0100

    Enums für creatures, houses, InvidentSeverity angelegt.

---
+++ Enums/House.cs
@@ -0,0 +1,10 @@
+namespace HausSlytherin_SMIS.Enums
+{
+    public enum House
+    {
+        Gryffindor = 1,
+        Hufflepuff = 2,
+        Ravenclaw = 3,
+        Slytherin = 4
+    }
+}
---
+++ Enums/IncidentSeverity.cs
@@ -0,0 +1,12 @@
+namespace HausSlytherin_SMIS.Enums
+{
+    
+    public enum IncidentSeverity
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+    
+}
