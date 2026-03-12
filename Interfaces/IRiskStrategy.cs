9def4fb724891faa9be48aea611c63594521ce6d
PixelGG <mikephilipp3@gmail.com>
2026-03-12 14:05:06 +0100

    feat(risk-strategy): definiere Methoden für Risikoberechnung und Empfehlungen

--- Interfaces/IRiskStrategy.cs
+++ Interfaces/IRiskStrategy.cs
@@ -1,5 +1,20 @@
+using HausSlytherin_SMIS.Models;
+
 namespace HausSlytherin_SMIS.Interfaces;
 
 public interface IRiskStrategy
 {
+    /// <summary> Gibt den Anzeigenamen der Strategie zurueck. </summary>
+    string Name { get; }
+
+    /// <summary> Berechnet den Risikowert fuer eine Kreatur und einen Vorfall. </summary>
+    /// <param name="creature">Die zu bewertende Kreatur.</param>
+    /// <param name="incident">Der zu bewertende Vorfall.</param>
+    /// <returns>Der berechnete Risikowert.</returns>
+    int CalculateRisk(Creature creature, Incident incident);
+
+    /// <summary> Wandelt einen Risikowert in eine textuelle Empfehlung um. </summary>
+    /// <param name="riskScore">Der berechnete Risikowert.</param>
+    /// <returns>Eine Empfehlung zum Umgang mit dem Risiko.</returns>
+    string GetRecommendation(int riskScore);
 }
