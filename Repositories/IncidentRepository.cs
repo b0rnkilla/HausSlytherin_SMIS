c81e4b57f3f06827f4ea31a33e42bb4e644df598
PBSascha <pb-00-gebsas1@schulung-bbw.de>
2026-03-12 11:16:48 +0100

    feat: fügt IncidentRepository hinzu

--- Repositories/IncidentRepository.cs
+++ Repositories/IncidentRepository.cs
@@ -5,4 +5,47 @@ namespace HausSlytherin_SMIS.Repositories;
 
 public class IncidentRepository : IRepository<Incident>
 {
+    private readonly List<Incident> _incidents;
+
+    public IncidentRepository()
+    {
+        _incidents = new List<Incident>();
+    }
+
+    public void Add(Incident item)
+    {
+        _incidents.Add(item);
+    }
+
+    public void Remove(int id)
+    {
+        var incident = GetById(id);
+        if (incident != null)
+        {
+            _incidents.Remove(incident);
+        }
+    }
+
+    public IEnumerable<Incident> GetAll()
+    {
+        return _incidents;
+    }
+
+    public Incident GetById(int id)
+    {
+        return _incidents.FirstOrDefault(i => i.Id == id);
+    }
+
+    public void Update(Incident item)
+    {
+        var existingIncident = GetById(item.Id);
+        if (existingIncident != null)
+        {
+            existingIncident.Title = item.Title;
+            existingIncident.Description = item.Description;
+            existingIncident.Severity = item.Severity;
+            existingIncident.Date = item.Date;
+            existingIncident.CreatureId = item.CreatureId;
+        }
+    }
 }
