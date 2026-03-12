44a47c4a8719a913b6b28cae736fd89481a9d0a8
PBSascha <pb-00-gebsas1@schulung-bbw.de>
2026-03-12 11:57:31 +0100

    feat: fügt CreatureRepository hinzu

--- Repositories/CreatureRepository.cs
+++ Repositories/CreatureRepository.cs
@@ -1,7 +1,33 @@
 using HausSlytherin_SMIS.Interfaces;
+using HausSlytherin_SMIS.Models;
 
 namespace HausSlytherin_SMIS.Repositories;
 
 public class CreatureRepository : ICreatureRepository
 {
+    private readonly List<Creature> _creatures = new();
+
+    public void Add(Creature item)
+    {
+        _creatures.Add(item);
+    }
+
+    public List<Creature> GetAll()
+    {
+        return _creatures;
+    }
+
+    public Creature? GetById(int id)
+    {
+        return _creatures.FirstOrDefault(c => c.Id == id);
+    }
+
+    public void Remove(int id)
+    {
+        var creature = GetById(id);
+        if (creature != null)
+        {
+            _creatures.Remove(creature);
+        }
+    }
 }
