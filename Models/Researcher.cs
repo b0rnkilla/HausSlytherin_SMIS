7236b12f6314f5728c293e6d9b4be5e3d4bfc4af
Andreas Lorenz <andreas.lorenz1986@web.de>
2026-03-12 09:01:30 +0100

    Researcher Angelegt und mit Logik gefüllt

--- Models/Researcher.cs
+++ Models/Researcher.cs
@@ -2,4 +2,13 @@ namespace HausSlytherin_SMIS.Models;
 
 public class Researcher
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string House { get; set; } = string.Empty;
+    public string Specialization { get; set; } = string.Empty;
+    public int ExperienceLevel { get; set; }
+
+    public void PrintInfo() =>
+        Console.WriteLine($"Researcher: {Name} | House: {House} | Specialization: {Specialization} | Level: {ExperienceLevel} ");
+
 }
